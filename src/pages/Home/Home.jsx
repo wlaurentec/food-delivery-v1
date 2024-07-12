efacff3f1e31384c638531ae9be219379740ee25
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-12 10:01:25 -0500

    add explore menu styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
 import Header from "../../components/Header/Header"
 import "./Home.css"
+import { useState } from "react";
+
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All")
+
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
