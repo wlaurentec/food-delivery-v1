e4f3ad9e4a2adacbeb54ac32b6c721abe6d8ac2e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-12 07:44:25 -0500

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import NavBar from "./components/NavBar/NavBar"
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceOrder = () => {
+  return (
+    <div>
+      PlaceOrder
+    </div>
+  )
+}
+
+export default PlaceOrder
