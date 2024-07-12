879b4a214124c3ab951d39bd6879a58fb373915f
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-07-11 22:58:41 -0500

    add navbar styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,19 @@
 import "./NavBar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 const NavBar = () => {
+
+  const [menu, setMenu] = useState("home")
+
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact-us</li>
+        <li onClick={() => setMenu("home")} className={menu==="home" ? "active" : ""}>home</li>
+        <li onClick={() => setMenu("menu")} className={menu==="menu" ? "active" : ""}>menu</li>
+        <li onClick={() => setMenu("mobile-app")} className={menu==="mobile-app" ? "active" : ""}>mobile-app</li>
+        <li onClick={() => setMenu("contact-us")} className={menu==="contact-us" ? "active" : ""}>contact-us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
